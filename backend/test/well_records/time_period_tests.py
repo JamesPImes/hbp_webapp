dc9720b2094ea3b653b21172e74cd930db8c2379
JamesPImes <jamesimes@gmail.com>
2024-03-25 01:16:12 -0700

    test: TimePeriod tests

---
+++ backend/test/well_records/time_period_tests.py
@@ -0,0 +1,32 @@
+
+import unittest
+from datetime import date
+
+from backend.well_records.time_period import TimePeriod
+
+
+START_DATE = date(2014, 1, 1)
+END_DATE = date(2015, 2, 1)
+EXPECTED_DAYS = 396
+EXPECTED_MONTHS = 13
+DATE_RANGE_AS_STRING = "2014-01-01::2015-02-01"
+CATEGORY = "TEST"
+
+
+class TestTimePeriod(unittest.TestCase):
+
+    def test_duration_in_days(self):
+        time_period = TimePeriod(START_DATE, END_DATE, CATEGORY)
+        self.assertEqual(time_period.duration_in_days(), EXPECTED_DAYS)
+
+    def test_duration_in_months(self):
+        time_period = TimePeriod(START_DATE, END_DATE, CATEGORY)
+        self.assertEqual(time_period.duration_in_months(), EXPECTED_MONTHS)
+
+    def test_from_string(self):
+        time_period = TimePeriod.from_string(DATE_RANGE_AS_STRING, category=CATEGORY)
+        self.assertEqual(time_period.duration_in_months(), EXPECTED_MONTHS)
+
+
+if __name__ == '__main__':
+    unittest.main()
