d8d7b543686aecf40947b65637abf2852a5fba6e
JamesPImes <jamesimes@gmail.com>
2024-03-25 02:22:56 -0700

    skeleton of WellGroup tests

---
+++ backend/test/data_analyzer/well_group_tests.py
@@ -0,0 +1,24 @@
+import unittest
+
+from backend.data_analyzer.well_group import WellGroup
+from backend.well_records.well_record import WellRecord
+from backend.well_records.time_period import TimePeriod
+
+
+class TestWellGroup(unittest.TestCase):
+
+    def test_add_well_record(self):
+        pass
+
+    def test_find_first_date(self):
+        pass
+
+    def test_find_last_date(self):
+        pass
+
+    def test_find_gaps(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
