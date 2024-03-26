592d1ab14c671742d3c1f6e0d935016e5803bea0
JamesPImes <jamesimes@gmail.com>
2024-03-25 18:04:07 -0700

    test: merge_with() and subtract_out()

--- backend/test/well_records/time_period_tests.py
+++ backend/test/well_records/time_period_tests.py
@@ -26,6 +26,34 @@ class TestTimePeriod(unittest.TestCase):
         time_period = TimePeriod.from_string(DATE_RANGE_AS_STRING, category=CATEGORY)
         self.assertEqual(time_period.duration_in_months(), EXPECTED_MONTHS)
 
+    def test_merge_with(self):
+        tp1 = TimePeriod(
+            start_date=date(2014, 1, 1), end_date=date(2015, 1, 31), category=CATEGORY
+        )
+        tp2 = TimePeriod(
+            start_date=date(2014, 12, 1), end_date=date(2015, 5, 31), category=CATEGORY
+        )
+        expected = TimePeriod(
+            start_date=date(2014, 1, 1), end_date=date(2015, 5, 31), category=CATEGORY
+        )
+        result = tp1.merge_with(tp2)
+        self.assertEqual(result.start_date, expected.start_date)
+        self.assertEqual(result.end_date, expected.end_date)
+
+    def test_subtract_out(self):
+        tp1 = TimePeriod(
+            start_date=date(2014, 1, 1), end_date=date(2015, 1, 31), category=CATEGORY
+        )
+        tp2 = TimePeriod(
+            start_date=date(2014, 12, 1), end_date=date(2015, 5, 31), category=CATEGORY
+        )
+        expected = TimePeriod(
+            start_date=date(2014, 1, 1), end_date=date(2014, 11, 30), category=CATEGORY
+        )
+        result = tp1.subtract_out(tp2)
+        self.assertEqual(result.start_date, expected.start_date)
+        self.assertEqual(result.end_date, expected.end_date)
+
 
 if __name__ == "__main__":
     unittest.main()
