ffb37d64782611dce7025c3c40a883befc833392
JamesPImes <jamesimes@gmail.com>
2024-03-25 17:10:37 -0700

    add docstrings

--- backend/data_analyzer/well_group.py
+++ backend/data_analyzer/well_group.py
@@ -9,9 +9,14 @@ class WellGroup:
         self.well_records: list[WellRecord] = []
 
     def add_well_record(self, well_record: WellRecord) -> None:
+        """Add a well record to this well group."""
         self.well_records.append(well_record)
 
     def find_first_date(self) -> date:
+        """
+        Find the earliest date in any of the well records.
+        Returns None if no added records have a first date specified.
+        """
         first = None
         for wr in self.well_records:
             if first is None:
@@ -21,6 +26,10 @@ class WellGroup:
         return first
 
     def find_last_date(self) -> date:
+        """
+        Find the latest date in any of the well records.
+        Returns None if no added records have a last date specified.
+        """
         last = None
         for wr in self.well_records:
             if last is None:
