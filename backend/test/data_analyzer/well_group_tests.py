c39c782f24deb10856bb4b1493125ec9713ff41f
JamesPImes <jamesimes@gmail.com>
2024-03-27 11:02:13 -0700

    clean up WellGroup tests

--- backend/test/data_analyzer/well_group_tests.py
+++ backend/test/data_analyzer/well_group_tests.py
@@ -33,7 +33,7 @@ class TestWellGroup(unittest.TestCase):
         )
         wg.add_well_record(wr1)
         wg.add_well_record(wr2)
-        self.assertEqual(wg.find_first_date(), expected_first_date)
+        self.assertEqual(expected_first_date, wg.find_first_date())
 
     def test_find_first_date_none(self):
         wg = WellGroup()
@@ -51,7 +51,7 @@ class TestWellGroup(unittest.TestCase):
         )
         wg.add_well_record(wr1)
         wg.add_well_record(wr2)
-        self.assertEqual(wg.find_first_date(), None)
+        self.assertEqual(None, wg.find_first_date())
 
     def test_find_last_date(self):
         wg = WellGroup()
@@ -70,7 +70,7 @@ class TestWellGroup(unittest.TestCase):
         )
         wg.add_well_record(wr1)
         wg.add_well_record(wr2)
-        self.assertEqual(wg.find_last_date(), expected_last_date)
+        self.assertEqual(expected_last_date, wg.find_last_date())
 
     def test_find_last_date_none(self):
         wg = WellGroup()
@@ -88,9 +88,13 @@ class TestWellGroup(unittest.TestCase):
         )
         wg.add_well_record(wr1)
         wg.add_well_record(wr2)
-        self.assertEqual(wg.find_last_date(), None)
+        self.assertEqual(None, wg.find_last_date())
 
-    def test_find_gaps(self):
+
+class TestWellGroup_gaps(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
         wr1 = WellRecord(
             api_num="05-123-45678",
             well_name="test well #1",
@@ -115,27 +119,26 @@ class TestWellGroup(unittest.TestCase):
         wg.add_well_record(wr1)
         wg.add_well_record(wr2)
 
-        gaps = wg.find_gaps(category="test")
-        dr_expected = DateRange(start_date=date(2002, 5, 1), end_date=date(2003, 12, 31))
-        # Only 1 gap remains.
-        self.assertEqual(
-            len(gaps),
-            1,
-            f"Expected 1 gap to remain for category 'test'; found {len(gaps)}.",
+        cls.gaps = wg.find_gaps(category="test")
+        cls.dr_expected = DateRange(
+            start_date=date(2002, 5, 1), end_date=date(2003, 12, 31)
         )
-        gap = gaps[0]
+
+    def test_find_gaps_number_found(self):
+        self.assertEqual(len(self.gaps), 1)
+        gap = self.gaps[0]
         # Start dates match.
-        self.assertEqual(
-            gap.start_date,
-            dr_expected.start_date,
-            f"Expected start date of {dr_expected.start_date}; found {gap.start_date}.",
-        )
+        self.assertEqual(self.dr_expected.start_date, gap.start_date)
         # End dates match.
-        self.assertEqual(
-            gap.end_date,
-            dr_expected.end_date,
-            f"Expected end date of {dr_expected.end_date}; found {gap.end_date}.",
-        )
+        self.assertEqual(self.dr_expected.end_date, gap.end_date)
+
+    def test_find_gaps_start_date(self):
+        gap = self.gaps[0]
+        self.assertEqual(self.dr_expected.start_date, gap.start_date)
+
+    def test_find_gaps_end_date(self):
+        gap = self.gaps[0]
+        self.assertEqual(self.dr_expected.end_date, gap.end_date)
 
 
 if __name__ == "__main__":
