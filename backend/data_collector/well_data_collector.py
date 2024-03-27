7e96400242a0df291717b6dad364a27858681843
JamesPImes <jamesimes@gmail.com>
2024-03-27 01:20:05 -0700

    feat: data collector interface

---
+++ backend/data_collector/well_data_collector.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+from ..well_records.well_record import WellRecord
+
+
+class WellDataCollector(ABC):
+    """Interface for collector of well data."""
+
+    @abstractmethod
+    def get_well_data(self, api_num: str, well_name: str, **kw) -> WellRecord:
+        raise NotImplementedError
+
+
+__all__ = [
+    "WellDataCollector",
+]
