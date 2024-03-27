e9a2eef75da183f964560241a112201057516792
JamesPImes <jamesimes@gmail.com>
2024-03-26 23:52:58 -0700

    explaining initial categories

--- backend/well_records/standard_categories.py
+++ backend/well_records/standard_categories.py
@@ -1,3 +1,8 @@
 
+# Months during which there is no production; and ignoring shut-in
+# status codes.
 NO_PROD_IGNORE_SHUTIN = "NO_PROD_IGNORE_SHUTIN"
+
+# Months during which there is no production; but if a well is shut-in,
+# it counts as producing.
 NO_PROD_BUT_SHUTIN_COUNTS = "NO_PROD_BUT_SHUTIN_COUNTS"
