60d4b5e7bfa58dcd6e711c62606ff87a779c2a21
JamesPImes <jamesimes@gmail.com>
2024-04-03 10:25:53 -0700

    pull records from the database where possible

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask, request
+import os
 
+import dotenv
+from flask import Flask, request
+from pymongo import MongoClient
+
+from backend.database.mongodb_well_record_manager import MongoDBWellRecordManager
 from backend.data_analyzer.well_group import WellGroup
 from backend.well_records.standard_categories import NO_PROD_IGNORE_SHUTIN
 from backend.data_collector.well_data_scraper import ScraperWellDataCollector
 from backend.data_collector.state_configs.colorado import COLORADO_CONFIG
+
+
+dotenv.load_dotenv(".env")
+
+DATABASE_CONNECTION_STRING = os.environ.get("DATABASE_CONNECTION_STRING")
+DATABASE_NAME = os.environ.get("DATABASE_NAME")
+WELL_RECORDS_COLLECTION_NAME = os.environ.get("WELL_RECORDS_COLLECTION_NAME")
+
+CONNECTION = MongoClient(DATABASE_CONNECTION_STRING)
 
 app = Flask(__name__)
 
@@ -21,15 +35,26 @@ def main():
 @app.route("/collect_wells", methods=["POST"])
 def collect_wells_and_find_gaps():
     input_text = request.form.get("api_nums", "")
-
     api_nums = input_text.split(",")
+
+    wrm = MongoDBWellRecordManager(
+        connection=CONNECTION,
+        db_name=DATABASE_NAME,
+        well_records_collection_name=WELL_RECORDS_COLLECTION_NAME,
+    )
 
     scraper = ScraperWellDataCollector.from_config(COLORADO_CONFIG)
 
     wg = WellGroup()
     for num in api_nums:
-        new_data = scraper.get_well_data(api_num=num)
-        wg.add_well_record(new_data)
+        well_record = wrm.find_well_record(num)
+        if well_record is None:
+            well_record = scraper.get_well_data(api_num=num)
+            print(f"scraping {num}")
+            wrm.insert_well_record(well_record)
+        else:
+            print(f"pulled {num} from database")
+        wg.add_well_record(well_record)
 
     gaps = wg.find_gaps(NO_PROD_IGNORE_SHUTIN)
     output_str = f"for wells in {input_text}:"
