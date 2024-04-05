1afe02159d295487262cd8ed6bb23b3495af8041
JamesPImes <jamesimes@gmail.com>
2024-04-05 02:51:32 -0700

    specify expected params

--- backend/database/well_record_manager.py
+++ backend/database/well_record_manager.py
@@ -5,16 +5,16 @@ from backend.well_records.well_record import WellRecord
 
 class WellRecordManager(ABC):
     """Interface for well record manager."""
-    def insert_well_record(self, well_record: WellRecord) -> None:
+    def insert_well_record(self, well_record: WellRecord, **kw) -> None:
         raise NotImplementedError
 
-    def find_well_record(self, **kw) -> WellRecord:
+    def find_well_record(self, api_num, **kw) -> WellRecord:
         raise NotImplementedError
 
-    def update_well_record(self, well_record) -> None:
+    def update_well_record(self, well_record, **kw) -> None:
         raise NotImplementedError
 
-    def delete_well_record(self, **kw) -> None:
+    def delete_well_record(self, api_num, **kw) -> None:
         raise NotImplementedError
 
 
