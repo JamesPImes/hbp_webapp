4e92d3dca07d85d4e5ca0ff480a557b2211b5ff7
JamesPImes <jamesimes@gmail.com>
2024-04-07 00:58:46 -0700

    refactor: move category descriptions to backend.well_records.standard_categories

--- backend/well_records/__init__.py
+++ backend/well_records/__init__.py
@@ -1,3 +1,7 @@
 from .date_range import DateRange, DateRangeGroup
-from .standard_categories import NO_PROD_IGNORE_SHUTIN, NO_PROD_BUT_SHUTIN_COUNTS
+from .standard_categories import (
+    NO_PROD_IGNORE_SHUTIN,
+    NO_PROD_BUT_SHUTIN_COUNTS,
+    CATEGORY_DESCRIPTIONS,
+)
 from .well_record import WellRecord
