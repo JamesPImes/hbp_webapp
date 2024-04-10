9630ae807de95f646d84b10190bc4d5569fedc5b
JamesPImes <jamesimes@gmail.com>
2024-04-10 00:04:00 -0700

    mark WellRecordGateway methods as abstract methods

--- backend/database/well_record_data_gateway.py
+++ backend/database/well_record_data_gateway.py
@@ -1,19 +1,24 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from backend.well_records import WellRecord
 
 
 class WellRecordDataGateway(ABC):
     """Interface for well record data gateway."""
+
+    @abstractmethod
     def insert(self, well_record: WellRecord, **kw) -> None:
         raise NotImplementedError
 
+    @abstractmethod
     def find(self, api_num, **kw) -> WellRecord:
         raise NotImplementedError
 
+    @abstractmethod
     def update(self, well_record, **kw) -> None:
         raise NotImplementedError
 
+    @abstractmethod
     def delete(self, api_num, **kw) -> None:
         raise NotImplementedError
 
