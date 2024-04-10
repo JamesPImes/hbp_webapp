9edfb51af433b04e96cf3554d7d457a370070cf3
JamesPImes <jamesimes@gmail.com>
2024-04-10 01:11:31 -0700

    abandon 'get gateway' func

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -1,7 +1,4 @@
 from .mongodb_loader import get_mongo_client_for_environment
 from .mongodb_manager import MongoDBManager
-from .mongodb_well_record_data_gateway import (
-    MongoDBWellRecordDataGateway,
-    get_well_record_gateway_for_environment,
-)
+from .mongodb_well_record_data_gateway import MongoDBWellRecordDataGateway
 from .well_record_data_gateway import WellRecordDataGateway
