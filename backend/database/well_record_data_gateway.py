30c64c1bdd836a2d1a56f642d23ea03565bc693f
JamesPImes <jamesimes@gmail.com>
2024-04-10 01:08:14 -0700

    Revert "mark WellRecordGateway methods as abstract methods"
    This reverts commit 9630ae807de95f646d84b10190bc4d5569fedc5b.

--- backend/database/well_record_data_gateway.py
+++ backend/database/well_record_data_gateway.py
@@ -1,24 +1,19 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 
 from backend.well_records import WellRecord
 
 
 class WellRecordDataGateway(ABC):
     """Interface for well record data gateway."""
-
-    @abstractmethod
     def insert(self, well_record: WellRecord, **kw) -> None:
         raise NotImplementedError
 
-    @abstractmethod
     def find(self, api_num, **kw) -> WellRecord:
         raise NotImplementedError
 
-    @abstractmethod
     def update(self, well_record, **kw) -> None:
         raise NotImplementedError
 
-    @abstractmethod
     def delete(self, api_num, **kw) -> None:
         raise NotImplementedError
 
