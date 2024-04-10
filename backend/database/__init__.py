952d83680e93046663514854d187478325c2def2
JamesPImes <jamesimes@gmail.com>
2024-04-10 01:14:19 -0700

    abandon 'get for environ' func

--- backend/database/__init__.py
+++ backend/database/__init__.py
@@ -1,4 +1,3 @@
-from .mongodb_loader import get_mongo_client_for_environment
 from .mongodb_manager import MongoDBManager
 from .mongodb_well_record_data_gateway import MongoDBWellRecordDataGateway
 from .well_record_data_gateway import WellRecordDataGateway
