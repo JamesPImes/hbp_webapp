cfd1fe27febabe700f16a3923005fb899a44e5d1
James Imes <jamesimes@gmail.com>
2025-03-26 17:19:39 -0600

    optional HOST and PORT env args

--- app.py
+++ app.py
@@ -2,10 +2,13 @@
 Create app instance for deployment.
 """
 
+import os
+import dotenv
 from create_app import Config, create_app
 
+dotenv.load_dotenv()
 
 app = create_app(Config())
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host=os.environ.get("HOST"), port=os.environ.get("PORT"), load_dotenv=True)
